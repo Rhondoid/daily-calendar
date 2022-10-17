68e902447ca5e0b5aa6cc505ca52121edee8a13f
Rhonda <110504360+Rhondoid@users.noreply.github.com>
2022-10-16 19:40:21 -0600

    added script.js and moment.js

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+
+
+
+var today =$moment(.format();
+
+
+var timeBlock = "textarea";
+var time = ["past","present", "future"];
+var textArea ="add event";
+var currentTime= "hour";
+var currentDay ="today";
+
+
+
+let m= moment();
+console.log(m);
